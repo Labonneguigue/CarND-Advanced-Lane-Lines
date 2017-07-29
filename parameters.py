715f67727b742260eef7f35a36d85cad5c60cbc8
Labonneguigue <granie.guillaume@gmail.com>
2017-07-29 11:59:23 +0100

    Visually correct parameters for the perspective transform

--- parameters.py
+++ parameters.py
@@ -2,7 +2,7 @@
 Set of parameters
 Keeping them here facilitates tunning my processing pipeline
 '''
-parameters = { 'orig_points_x' : (585, 695, 1127, 203),#(617, 660, 1125, 188),
+parameters = { 'orig_points_x' : (575, 705, 1127, 203),#(617, 660, 1125, 188),
                'orig_points_y' : 460, #434,
                's_thresh'      : (170, 255),
                'sx_thresh'     : (20, 100)
